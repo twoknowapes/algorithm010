6b280719f6014fdbfd7324e54d3da6d8d430ca17
makey.yang <1007845131@qq.com>
2020-06-12 14:40:25 +0800

    Design circularDeque

---
+++ Week01/design-circular-deque.py
@@ -0,0 +1,31 @@
+import collections
+
+
+class MyCircularDeque:
+    def __init__(self, k: int):
+       self.deque = collections.deque(maxlen=k) 
+
+    def insertFront(self, value: int) -> bool:
+        return len(self.deque) < self.deque.maxlen and (self.deque.appendleft(value) or True)      
+
+    def insertLast(self, value: int) -> bool:
+        return len(self.deque) < self.deque.maxlen and (self.deque.append(value) or True)
+
+    def deleteFront(self) -> bool:   
+        return self.deque and (self.deque.popleft() or True)
+
+    def deleteLast(self) -> bool:
+        return self.deque and (self.deque.pop() or True)
+
+    def getFront(self) -> int:    
+        return not self.deque and -1 or self.deque[0]
+
+    def getRear(self) -> int:
+        return not self.deque and -1 or self.deque[-1]
+
+    def isEmpty(self) -> bool:
+        return not self.deque
+
+    def isFull(self) -> bool:
+        return len(self.deque) == self.deque.maxlen
+
