c0e46d020942b840848b6c046dbd153859c9573c
makey.yang <1007845131@qq.com>
2020-06-18 10:22:19 +0800

    Modified File

--- Week01/trapping-rain-water.py
+++ Week01/trapping-rain-water.py
@@ -6,7 +6,7 @@ def trap(self, height: list[int]) -> int:
         while stacks and height[i] >= height[stacks[-1]]:
             h = stacks.pop()
             if not stacks: break
-            res += (min(height[i], height[stacks[-1]]) - height[h] * (i - stacks[-1] - 1)
-        stacks.append(i)
-
+            res += (min(height[i], height[stacks[-1]]) - height[h]) * (
+                        i - stacks[-1] - 1)
+            stacks.append(i)
     return res
